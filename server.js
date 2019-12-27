d924f9219b074ed9a8f88d66e0d341d01dc9af9b
prathyusha98-osos <59284060+prathyusha98-osos@users.noreply.github.com>
2019-12-27 18:08:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ var Customer=require('./models/Contacts');
 
 
 //  var User=require('./Users')
-app.listen(3000,()=>{
+app.listen(process.env.PORT|| 3000,()=>{
   console.log("start the server with port 3000")
 })
 
